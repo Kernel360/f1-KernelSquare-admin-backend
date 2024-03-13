502c1e40aefb773a3d505f8115da2dacc1e4d706
mooncw <dusrktl4@gmail.com>
2024-03-13 16:16:27 +0900

    [setting] 관리자 서버 세팅 값 Jasypt 사용

--- admin-api/src/main/java/com/kernelsquare/adminapi/common/config/RedisConfig.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/common/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.kernelsquare.memberapi.common.config;
+package com.kernelsquare.adminapi.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
