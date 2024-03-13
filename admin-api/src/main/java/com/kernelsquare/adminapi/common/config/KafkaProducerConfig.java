851cfed62e56ae4a44a135403b265f88b20f8a19
mooncw <dusrktl4@gmail.com>
2024-03-13 11:20:32 +0900

    [setting] 관리자 서버에 맞게 세팅 1차

--- admin-api/src/main/java/com/kernelsquare/adminapi/common/config/KafkaProducerConfig.java
+++ admin-api/src/main/java/com/kernelsquare/adminapi/common/config/KafkaProducerConfig.java
@@ -1,4 +1,4 @@
-package com.kernelsquare.memberapi.common.config;
+package com.kernelsquare.adminapi.common.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
